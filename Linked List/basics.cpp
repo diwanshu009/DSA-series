276ee42c6041ee4d545aab0c58635146625fee3d
diwanshu009 <goddiwanshu@gmail.com>
2024-01-12 23:09:28 +0530

    Linked list basics

---
+++ Linked List/basics.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// LinkedList can be defined as the collection of nodes where each node contains a data field and a reference to next node!
+// It is also a linear data structure but elements are not stored in contiguous manner!
+// It is of 4 types : Apne aap padh lena!!
+// Magic line ⭐️ : LinkedList is Hindi!!
+
+// Structure of node
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int d){
+        this->data = d;
+        this->next = NULL;
+    }
+};
+
+int main(){
+    Node* head = new Node(-1);
+    Node* temp = head;
+    int n;
+    cin >> n;
+    for(int i=1;i<=n;i++){
+        int el;
+        cin >> el;
+        Node* newNode = new Node(el);
+        temp->next = newNode;
+        temp = newNode;
+    }
+    Node* curr = head;
+    while(curr){
+        cout << curr->data << " ";
+        curr = curr->next;
+    }
+    cout << endl;
+}
