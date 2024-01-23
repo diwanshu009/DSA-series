545032122c4d9b23107450757055c14ba9c8bcf7
diwanshu009 <115030243+diwanshu009@users.noreply.github.com>
2024-01-23 18:22:02 +0000

    next smaller element

--- Stack/Level-1.cpp
+++ Stack/Level-1.cpp
@@ -62,7 +62,7 @@ public:
     }
 };
 
-// longest valid parantheses (HARD)
+// ⭐️ longest valid parantheses (HARD)
 int longestValidParentheses(string str){
     if(str.size()==0) return 0;
     stack<int>st;
@@ -81,6 +81,22 @@ int longestValidParentheses(string str){
         }
     }
     return len;
+}
+
+// next smaller element
+vector<int> nextSmallerElement(vector<int> &arr, int n){
+    stack<int>st;
+    st.push(-1);
+    vector<int>ans(n);
+    for(int i=n-1;i>=0;i--){
+        int curr = arr[i];
+        while(st.top()>=arr[i]){
+            st.pop();
+        }
+        ans[i]=st.top();
+        st.push(curr);
+    }
+    return ans;
 }
 
 int main(){
