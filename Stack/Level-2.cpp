7403b2ca5e9299617eaa744c957b2d82e1fca3f3
diwanshu009 <115030243+diwanshu009@users.noreply.github.com>
2024-01-27 12:08:26 +0000

    Stack assignment questsions done

--- Stack/Level-2.cpp
+++ Stack/Level-2.cpp
@@ -85,6 +85,112 @@ int celebrity(vector<vector<int> >& M, int n) {
     return celebrity;
 }
 
+// ⭐️ N Stacks In an Array (HARD)
+class NStack{
+
+    public:
+    int *a,*top,*next;
+    int freespot;
+    int n;
+    int size;
+
+    NStack(int N, int S){
+        this->n = N;
+        this->size = S;
+        freespot = 0;
+        a = new int[size];
+        top = new int[n];
+        next = new int[size];
+        for(int i=0;i<n;i++){
+            top[i]=-1;
+        }
+        for(int i=0;i<size;i++){
+            next[i] = i+1;
+        }
+        next[size-1] = -1;
+    }
+
+    bool push(int x, int m){
+        if(freespot==-1){
+            return false;
+        }
+        int index = freespot;
+        freespot = next[index];
+        a[index] = x;
+        next[index] = top[m-1];
+        top[m-1] = index;
+        return true;
+    }
+
+    int pop(int m){
+        if(top[m-1]==-1){
+            return -1;
+        }
+        int index = top[m-1];
+        top[m-1] = next[index];
+        int ans = a[index];
+        next[index] = freespot;
+        freespot = index;
+        return ans;
+    }
+};
+
+// online stack span (monotonic stack!)
+class StockSpanner {
+public:
+    stack<pair<int,int>>st;
+
+    StockSpanner() {
+        
+    }
+    int next(int price) {
+        int span = 1;
+        while(!st.empty() && st.top().first<=price){
+            span += st.top().second;
+            st.pop();
+        }
+        st.push({price,span});
+        return span;
+    }
+};
+
+// Do try simplify path and decode string questions!
+
+// Car fleet ⭐️
+class Solution {
+    class Car{
+        public:
+        int pos;
+        int speed;
+        Car(int pos,int speed){
+            this->pos = pos;
+            this->speed = speed;
+        }
+    };
+    static bool comp(Car& a,Car& b){
+        return a.pos < b.pos;
+    }
+
+public:
+    int carFleet(int target, vector<int>& position, vector<int>& speed) {
+        vector<Car>car;
+        for(int i=0;i<position.size();i++){
+            Car c(position[i],speed[i]);
+            car.push_back(c);
+        }
+        sort(car.begin(),car.end(),comp);
+        stack<float>st;
+        for(auto c:car){
+            float time = (target-c.pos)/((float)c.speed);
+            while(!st.empty() && time>=st.top()){
+                st.pop();
+            }
+            st.push(time);
+        }
+        return st.size();
+    }
+};
+
 // LRU implementataion!
 
 
