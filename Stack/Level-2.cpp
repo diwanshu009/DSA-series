988f23fac6b5477b513262bf19cfb73b4c0b56e9
diwanshu009 <115030243+diwanshu009@users.noreply.github.com>
2024-01-29 17:47:17 +0000

    Stack finished

--- Stack/Level-2.cpp
+++ Stack/Level-2.cpp
@@ -325,6 +325,36 @@ public:
     }
 };
 
+// ⭐️ Remove duplicate letters
+string removeDuplicateLetters(string s) {
+    stack<char>st;
+    string ans = "";
+    vector<bool>seen(26,0);
+    unordered_map<char,int>mp;
+    for(int i=0;i<s.size();i++){
+        mp[s[i]]++;
+    }
+    for(int i=0;i<s.size();i++){
+        if(seen[s[i]-'a']){
+            mp[s[i]]--;
+            continue;
+        }
+        while(!st.empty() && st.top()>s[i] && mp[st.top()]!=0){
+            seen[st.top()-'a'] = 0;
+            st.pop();
+        }
+        mp[s[i]]--;
+        seen[s[i]-'a'] = 1;
+        st.push(s[i]);
+    }
+    while(st.size()){
+        ans += st.top();
+        st.pop();
+    }
+    reverse(ans.begin(),ans.end());
+    return ans;
+}
+
 
 int main(){
 
