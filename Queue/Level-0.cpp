6e6d91e4199527e6fcf4a9aa4e0ef25b83780baf
diwanshu009 <115030243+diwanshu009@users.noreply.github.com>
2024-01-29 18:40:47 +0000

    Implementation done

--- Queue/Level-0.cpp
+++ Queue/Level-0.cpp
@@ -10,7 +10,37 @@ Implementaion : 1. Using STL( #include<queue> )
 using namespace std;
 
 // implementation of queue using arrays with classes!
+class Queue {
 
+	int front, rear;
+	vector<int> arr;
+
+    public:
+
+	Queue(){
+		front = -1;
+		rear = -1;
+		arr.resize(100001);
+	}
+
+	void enqueue(int e){
+		if(front == -1 || rear == -1){
+			front = 0;
+			rear = 0;
+		}
+		arr[rear] = e;
+		rear++;
+	}
+    
+	int dequeue(){
+		if(front == rear || front == -1){
+			return -1;
+		}
+		int ans = arr[front];
+		front++;
+		return ans;
+	}
+};
 
 int main(){
 
