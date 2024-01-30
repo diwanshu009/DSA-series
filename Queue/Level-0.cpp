58be8273da620484b0aa96038dcb83f8a280f41b
diwanshu009 <115030243+diwanshu009@users.noreply.github.com>
2024-01-30 18:26:18 +0000

    circular queue implementation

--- Queue/Level-0.cpp
+++ Queue/Level-0.cpp
@@ -24,6 +24,7 @@ class Queue {
 	}
 
 	void enqueue(int e){
+		// can also check whether the queue is full or not!
 		if(front == -1 || rear == -1){
 			front = 0;
 			rear = 0;
@@ -40,6 +41,53 @@ class Queue {
 		front++;
 		return ans;
 	}
+};
+
+// circular queue
+class CircularQueue{
+    public:
+    int *arr;
+    int front;
+    int rear;
+    int size;
+    CircularQueue(int n){
+        size = n;
+        arr = new int[size];
+        front = rear = -1;
+    }
+
+    bool enqueue(int value){
+        if((front == 0 && rear == size -1) || ( (rear+1)%size == front) ){
+            return false;
+        }
+        else if(front == -1){
+            front = rear = 0;
+        } else if (rear == size - 1 && front != 0) {
+            rear = 0;
+        } else {
+            rear++;
+        }
+        arr[rear]= value;
+        return true;
+    }
+
+    int dequeue(){
+        if(front == -1){
+            return -1;
+        }
+        int ans = arr[front];
+        arr[front] = -1;
+        if(front == rear){
+            front = rear = -1;
+        }
+        else if(front == size-1){
+            front = 0;
+        }
+        else{
+            front++;
+        }
+        return ans;
+    }
 };
 
 int main(){
