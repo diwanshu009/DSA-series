7f0efca299d5385ffe9b41c41f65d9c8e920d3ed
diwanshu009 <115030243+diwanshu009@users.noreply.github.com>
2024-02-10 17:37:33 +0000

    Sliding Window Questions

--- Sliding Window/questions.cpp
+++ Sliding Window/questions.cpp
@@ -55,6 +55,29 @@ vector<int> findAnagrams(string s, string p) {
     return ans;
 }
 
+// ⭐️ Similar Question-> Permutation in String (Do Try!)
+
+// Frequency of the most frequent element after performing atmost k operations!
+int maxFrequency(vector<int>& nums, int k) {
+    sort(nums.begin(),nums.end());
+    int i = 0;
+    int j = 0;
+        long long int sum = 0;
+        int ans = 0;
+        while(j<nums.size()){
+            sum += nums[j];
+            long long int len = j-i+1;
+            while(nums[j] * len > sum + k){
+                sum -= nums[i];
+                i++;
+                len = j - i + 1;
+            }
+            ans = max(ans,j-i+1);
+            j++;
+        }
+        return ans;
+    }
+
 int main(){
 
 }
