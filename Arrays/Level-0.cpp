fbf2989b933aecd04db028c12be57c715814d122
diwanshu009 <115030243+diwanshu009@users.noreply.github.com>
2024-02-25 07:44:27 +0000

    Problem Solving

--- Arrays/Level-0.cpp
+++ Arrays/Level-0.cpp
@@ -4,7 +4,11 @@
 // Array is the list of elements of same type placed in a contiguous manner. They are always passed by Reference!
 
 // Basics--> Largest No. in array, Reverse an array (Two pointer approach), Linear Search
-// Set insertion --> T.C = O(NlogN) , S.C = O(N)!
+
+/* Set (store elements in a sorted manner!) insertion (for 1 element) -> T.C = O(logN), S.C = O(N), whereas
+   Unordered Set stores element in an unordered manner (not Sorted/Random) , so its T.C = O(1), S.C = O(N) */
+
+// Tip --> First think about the brute force solution and then try to optimise it further (In an interview also!)
 
 // ⭐️ Second largest in an array (Similarly, Second Smallest in an array)
 int largest(vector<int>& arr){
@@ -60,6 +64,25 @@ void moveZeroes(vector<int>& nums) {
 }
 
 // Do Try --> Merge 2 sorted Arrays (union of 2 sorted arrays) without using set Data structure!
+// Do Try --> Intersection of 2 sorted Arrays in T.C = O(n+m), S.C = O(1)!
+
+// ⭐️ Missing Number
+int missingNumber(vector<int>& nums) { // Method 1: Using XOR (Slightly Better!) , Method 2:  Missing number = n*(n+1)/2 - arraySum
+    int a = 0;
+    int b = 0;
+    for(int i=0;i<nums.size();i++){
+        a = a ^ (i+1);
+        b = b ^ nums[i];
+    }
+    return a^b;
+}
+
+// Do Try --> Maximum Consecutive Ones!
+
+// Do Try --> Find the number that appear once and others twice (Using XOR it can be easily done, ans = ans ^ arr[i])!
+
+/* ⭐️ Important note :  1. The time complexity of unordered_map operations is O(1) on average. S.C. = O(N)
+2. The time complexity of map operation (for 1 element) is O(log n) as elements are arranged in sorted manner, S.C = O(N)
 
 int main(){
 
