fca6878936b89cf7a15a10ccb6d1c40549601a23
diwanshu009 <115030243+diwanshu009@users.noreply.github.com>
2024-03-02 11:53:36 +0000

    Merge Overlapping intervals

--- Arrays/Level-2.cpp
+++ Arrays/Level-2.cpp
@@ -147,3 +147,17 @@ int subarraysWithSumK(vector < int > a, int b) {
     }
     return ans;
 }
+
+// ⭐️ Merge Overlapping Intervals!
+vector<vector<int>> mergeOverlappingIntervals(vector<vector<int>> &arr){
+	vector<vector<int>>ans;
+	sort(arr.begin(),arr.end());
+	for(int i=0;i<arr.size();i++){
+		if(ans.empty() || arr[i][0]>ans.back()[1]){
+			ans.push_back(arr[i]);
+		}else{
+			ans.back()[1] = max(ans.back()[1],arr[i][1]);
+		}
+	}
+	return ans;
+}
