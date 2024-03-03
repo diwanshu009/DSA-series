88d0cb05d457f8ede4f4499ab35fc4c14dc2891e
diwanshu009 <115030243+diwanshu009@users.noreply.github.com>
2024-03-03 09:37:01 +0000

    Finishing level 2

--- Arrays/Level-2.cpp
+++ Arrays/Level-2.cpp
@@ -230,3 +230,59 @@ vector<int> repeatedNumber(const vector<int> &arr) {
     int y = x-val1;
     return {x,y};
 }
+
+// Do Try --> Count Inversions (merge Sort me, ans += mid-left+1 krdo if element present in 2nd array is smaller!)
+
+// ⭐️ Reverse pairs (HARD!) 
+int ans = 0;
+
+void merge(vector<int>&arr,int s,int mid ,int e){
+    vector<int>temp;
+    int left = s;
+    int right = mid+1;
+    int i = left;
+    int j = right;
+    while(i<=mid){
+        if(j<=e && arr[i]>(long long)2*arr[j]){
+            j++;
+        }else{
+            ans += j-right;
+            i++;
+        }
+    }
+    while(left<=mid && right<=e){
+        if(arr[left]<=arr[right]){
+            temp.push_back(arr[left]);
+            left++;
+        }else{
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+    while(left<=mid){
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while(right<=e){
+        temp.push_back(arr[right]);
+        right++;
+    }
+    for(int i=s;i<=e;i++){
+        arr[i] = temp[i-s];
+    }
+}
+
+void help(vector<int>&arr,int s,int e){
+    if(s>=e) return;
+    int mid = s+(e-s)/2;
+    help(arr,s,mid);
+    help(arr,mid+1,e);
+    merge(arr,s,mid,e);
+}
+
+int reversePairs(vector<int>& arr) {
+    help(arr,0,arr.size()-1);
+    return ans;
+}
+
+// ⭐️ Maximum product Subarray!
