c46676479bc0009c2de1017c3155bfceb1c46395
diwanshu009 <115030243+diwanshu009@users.noreply.github.com>
2024-03-04 10:12:18 +0000

    Arrays finished

--- Arrays/Level-2.cpp
+++ Arrays/Level-2.cpp
@@ -286,3 +286,17 @@ int reversePairs(vector<int>& arr) {
 }
 
 // ⭐️ Maximum product Subarray!
+int maxProduct(vector<int>& nums) {
+    int n = nums.size();
+    int s = 1;
+    int p = 1;
+    int ans = INT_MIN;
+    for(int i=0;i<n;i++){
+        if(p==0) p = 1;
+        if(s==0) s = 1;
+        p *= nums[i];
+        s *= nums[n-i-1];
+        ans = max(ans,max(p,s));
+    }
+    return ans;
+}
