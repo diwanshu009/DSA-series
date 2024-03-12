773d543e060dc8b8d80f743e010147d197c05bb8
diwanshu009 <115030243+diwanshu009@users.noreply.github.com>
2024-03-12 18:04:39 +0000

    Greedy started

---
+++ Miscellaneous/Greedy.cpp
@@ -0,0 +1,15 @@
+/* Greedy algorithms are a class of algorithms that make locally optimal choices at each step with the hope of 
+  finding a global optimum solution. In these algorithms, decisions are made based on the information available at the 
+  current moment without considering the consequences of these decisions in the future. The key idea is to select the
+  best possible choice at each step, leading to a solution that may not always be the most optimal but is often good 
+  enough for many problems.
+*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+int main(){
+
+}
