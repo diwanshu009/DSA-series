c1baf4c2886c17b39b628e79767f0a02db918a35
diwanshu009 <115030243+diwanshu009@users.noreply.github.com>
2024-03-18 06:56:33 +0000

    Level-0 finished

--- BInary Search/Level-0.cpp
+++ BInary Search/Level-0.cpp
@@ -121,6 +121,56 @@ int findMin(vector<int>& arr){
 
 // No of times the array has been rotated --> Index of minimum number;
 
+// Single element in a Sorted Array (B.S!)
+int singleNonDuplicate(vector<int>& arr){
+    int s = 0;
+    int e = arr.size()-1;
+    int mid = s+(e-s)/2;
+    while(s<e){
+        if(mid%2!=0){
+            if(arr[mid-1]==arr[mid]){
+                s = mid+1;
+            }else{
+                e = mid-1;
+            }
+        }else{
+            if(mid<=e && arr[mid+1]==arr[mid]){
+                s = mid+1;
+            }else{
+                e = mid;
+            }
+        }
+        mid = s+(e-s)/2;
+    }
+    return arr[s];
+}
+
+// ⭐️ Peak element -->
+int findPeakElement(vector<int>& nums) {
+    int n = nums.size();
+    if(n == 1){
+        return 0;
+    }
+    if(nums[0]>nums[1]){
+        return 0;
+    }
+    if(nums[n-1]>nums[n-2]){
+        return n-1;
+    }
+    int s = 0;
+    int e = n-1;
+    int mid = s+(e-s)/2;
+    while(s<e){
+        if(nums[mid]<nums[mid+1]){
+            s = mid+1;
+        }else{
+            e = mid;
+        }
+        mid = s+(e-s)/2;
+    }
+    return s;
+}
+
 int main(){
     
 }
