d3a6904f2a4edb047898b91643f228039150a22a
diwanshu009 <115030243+diwanshu009@users.noreply.github.com>
2024-04-13 19:09:56 +0000

    Strings questions done

---
+++ Miscellaneous/Strings.cpp
@@ -0,0 +1,37 @@
+// Must to do questions for Strings -->
+
+#include<bits/stdc++.h>
+using namespace std;
+
+// Remove duplicates in string 
+
+// Remove occurences of a substring (using find function)
+
+// ⭐️ Palindromic substrings 
+
+// Valid palindrome (only 1 removal)
+
+// ⭐️ Longest common prefix
+
+// ⭐️ Isomorphic string
+
+// ⭐️ Valid anagram and Group anagram
+
+// ⭐️ Integer to roman or vice versa
+
+// ⭐️ Implementing atoi --> (string to integer)
+
+// ⭐️ Reorganise string
+
+// ⭐️ Zig zag conversion
+
+// Sliding window patterns
+
+/* Other Algos to do : 1. Rabin karp 
+2. Z function
+3. KMP algorithm
+*/
+
+int main(){
+
+}
