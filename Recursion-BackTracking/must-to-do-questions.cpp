50bbc680a4727f9e20247f43f67f035c4260d21c
diwanshu009 <115030243+diwanshu009@users.noreply.github.com>
2024-05-05 17:21:05 +0000

    Recursion-Backtacking done

---
+++ Recursion-BackTracking/must-to-do-questions.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// subsequence pattern ⭐️
+
+// combination sum questions ⭐️
+
+// House Robber questions ⭐️
+
+// Permutations pattern ⭐️
+
+// Rat in maze, sudoku-solver , N-queeen ⭐️
+
+// Generate paranthesis , letter combination of keypad ⭐️
+
+// Palindrome partitioning, Kth permutation, Distribute repeating integers, etc. ⭐️
+
+// Other will be covered in DP!!!!
+
+int main(){
+
+}
